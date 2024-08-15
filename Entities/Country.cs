bc03ebaaf1f5bb65b828fdf4e25ae571b83c15e7
Clever Cottonmouth <mirajhad@users.noreply.github.com>
2024-08-15 22:02:38 +0530

    dto added

---
+++ Entities/Country.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Country
+    {
+        public Guid CountryID { get; set; }
+        public string? CountryName { get; set; }
+    }
+}
