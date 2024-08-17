85dce23916067acaa262f15221b74c61bf82f127
Clever Cottonmouth <mirajhad@users.noreply.github.com>
2024-08-17 18:07:42 +0530

    validations

--- ServiceContracts/DTO/PersonAddRequest.cs
+++ ServiceContracts/DTO/PersonAddRequest.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using Entities;
+using System.ComponentModel.DataAnnotations;
 namespace ServiceContracts.DTO
 {
     /// <summary>
@@ -13,8 +14,13 @@ namespace ServiceContracts.DTO
     /// </summary>
     public class PersonAddRequest
     {
+        [Required(ErrorMessage = "Person Name can't be blank")]
         public string? PersonName { get; set; }
+
+        [Required(ErrorMessage = "Email can't be blank")]
+        [EmailAddress(ErrorMessage = "Email value should be a valid email")]
         public string? Email { get; set; }
+
         public DateTime? DateOfBirth { get; set; }
         public GenderOptions? Gender { get; set; }
         public Guid? CountryID { get; set; }
---
+++ Services/PersonsService.cs
@@ -0,0 +1,62 @@
+﻿using Entities;
+using ServiceContracts;
+using ServiceContracts.DTO;
+using Services.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Services
+{
+    public class PersonsService : IPersonsService
+    {
+        //private field
+        private readonly List<Person> _persons;
+        private readonly ICountriesService _countriesService;
+
+        public PersonsService()
+        {
+            _persons = new List<Person>();
+            _countriesService = new CountriesService();
+        }
+
+        private PersonResponse ConvertPersonToPersonResponse(Person person)
+        {
+            PersonResponse personResponse = person.ToPersonResponse();
+            personResponse.Country = _countriesService.GetCountryByCountryID(person.CountryID)?.CountryName;
+            return personResponse;
+        }
+
+        public PersonResponse AddPerson(PersonAddRequest? personAddRequest)
+        {
+            //check if PersonAddRequest is not null
+            if (personAddRequest == null)
+            {
+                throw new ArgumentNullException(nameof(personAddRequest));
+            }
+
+            //Model validation
+            ValidationHelper.ModelValidation(personAddRequest);
+
+
+            //convert personAddRequest into Person type
+            Person person = personAddRequest.ToPerson();
+
+            //generate PersonID
+            person.PersonID = Guid.NewGuid();
+
+            //add person object to persons list
+            _persons.Add(person);
+
+            //convert the Person object into PersonResponse type
+            return ConvertPersonToPersonResponse(person);
+        }
+
+        public List<PersonResponse> GetAllPersons()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
