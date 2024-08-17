da38a90ba76329ad63a5b1c0b0edfd36072b3144
Clever Cottonmouth <mirajhad@users.noreply.github.com>
2024-08-17 10:04:47 +0530

    add country fun

--- Services/CountriesService.cs
+++ Services/CountriesService.cs
@@ -1,4 +1,5 @@
-﻿using ServiceContracts;
+﻿using Entities;
+using ServiceContracts;
 using ServiceContracts.DTO;
 using System;
 using System.Collections.Generic;
@@ -10,9 +11,44 @@ namespace Services
 {
     public class CountriesService : ICountriesService
     {
+        private readonly List<Country> _countries;
+
+        public CountriesService()
+        {
+            _countries = new List<Country>();
+        }
+
         public CountryResponse AddCountry(CountryAddRequest? countryAddRequest)
         {
-            throw new NotImplementedException();
+
+            //Validation: countryAddRequest parameter can't be null
+            if (countryAddRequest == null)
+            {
+                throw new ArgumentNullException(nameof(countryAddRequest));
+            }
+
+            //Validation: CountryName can't be null
+            if (countryAddRequest.CountryName == null)
+            {
+                throw new ArgumentException(nameof(countryAddRequest.CountryName));
+            }
+
+            //Validation: CountryName can't be duplicate
+            if (_countries.Where(temp => temp.CountryName == countryAddRequest.CountryName).Count() > 0)
+            {
+                throw new ArgumentException("Given country name already exists");
+            }
+
+            //Convert object from CountryAddRequest to Country type
+            Country country = countryAddRequest.ToCountry();
+
+            //generate CountryID
+            country.CountryID = Guid.NewGuid();
+
+            //Add country object into _countries
+            _countries.Add(country);
+
+            return country.ToCountryResponse();
         }
     }
 }
