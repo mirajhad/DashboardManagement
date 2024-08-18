0fb075617089c22acd5231bd166a337083430654
Clever Cottonmouth <mirajhad@users.noreply.github.com>
2024-08-19 01:20:21 +0530

    ui updated

---
+++ DashboardManagement/Controllers/PersonsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DashboardManagement.Controllers
+{
+    public class PersonsController : Controller
+    {
+        [Route("persons/index")]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
